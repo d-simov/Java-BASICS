dd5b382ce51be9a17c659dc5eb8ac870689d8eb4
Dilyan Simov <115150533+d-simov@users.noreply.github.com>
2022-10-11 10:42:17 +0300

    Add files via upload

---
+++ Lab01ExcellentResult.java
@@ -0,0 +1,17 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class Lab01ExcellentResult {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+
+        if (a >= 5) System.out.println("Excellent!");
+
+
+    }
+}
